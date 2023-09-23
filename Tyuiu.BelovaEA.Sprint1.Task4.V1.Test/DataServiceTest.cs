2854ab5e74752861b6e15dea1b3b2debf5616bc8
Tyuiu.BelovaEA@outlook.com <belova.ek0@gmail,com>
2023-09-23 12:38:11 +0500

    Task4

---
+++ Tyuiu.BelovaEA.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BelovaEA.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.BelovaEA.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            DataService ds = new DataService();
+
+            double x = 2.0;
+
+            Assert.AreEqual(0.0625, ds.Calculate(x));
+        }
+    }
+}
---
+++ Tyuiu.BelovaEA.Sprint1.Task4.V1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BelovaEA.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.BelovaEA.Sprint1.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Белова Е. А. | ИИПб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                      *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Белова Екатерина Андреевна | ИИПб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("* Формула: 1 / (x + 2)^2                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите Х:                                                                *");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"1 / ({x} + 2)^2 = {ds.Calculate(x)}");
+
+            Console.ReadKey();
+        }
+    }
+}
